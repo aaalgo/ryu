7d497622680815175d0685d2acb13e6fa3012956
Wei Dong <wdong@wdong.org>
2016-10-07 16:04:10 -0400

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 good=1
+if [ ! -e dmdc ]
+then
+    echo dmdc directory not found, you need to create/symlink it.
+    good=
+fi
 if [ -z "$RYU_HOME" ]
 then
     echo 'Environment variable RYU_HOME should be set so that $RYU_HOME/init.sh is this file.'
@@ -13,14 +18,16 @@ then
 fi
 if docker --version >& /dev/null ; then true ; else
     echo missing docker
+    echo "https://docs.docker.com/engine/installation/"
     good=
 fi
 if nvidia-docker --version >& /dev/null ; then true; else
     echo "missing nvidia-docker"
+    echo "https://github.com/NVIDIA/nvidia-docker"
     good=
 fi
 if python3 --version >& /dev/null ; then true; else
-    echo "missing virtualenv"
+    echo "missing python3"
     good=
 fi
 if virtualenv --version >& /dev/null ; then true; else
@@ -49,10 +56,6 @@ echo "Logging into synapse"
 synapse login --rememberMe
 echo 
 echo "Logging into docker.synapse.org"
+echo "Use the same login credential as above"
 docker login docker.synapse.org
-
-if [ ! -e dmdc ]
-then
-    echo dmdc directory not found, you need to create/symlink it.
-fi
 
